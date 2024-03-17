9a125d0824a46c6cd78b10e96141a117309596af
Azmi SAHIN <azmisahin@outlook.com>
2024-03-17 14:36:01 +0000

    🚧: localizations for simulation particle

--- client/lib/features/simulation/options/simulation_particle_card.dart
+++ client/lib/features/simulation/options/simulation_particle_card.dart
@@ -3,6 +3,7 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:ui/repository/simulation_repository.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 class SimulationParticleCard extends StatefulWidget {
   final String title;
@@ -38,7 +39,11 @@ class _SimulationParticleCardState extends State<SimulationParticleCard> {
         _showErrorSnackbar(context, '$e');
       } else {
         // ignore: use_build_context_synchronously
-        _showErrorSnackbar(context, 'Failed to start simulation');
+        _showErrorSnackbar(
+            // ignore: use_build_context_synchronously
+            context,
+            // ignore: use_build_context_synchronously
+            AppLocalizations.of(context)!.simulation_particle_start_failed);
       }
     }
   }
@@ -55,7 +60,11 @@ class _SimulationParticleCardState extends State<SimulationParticleCard> {
         _showErrorSnackbar(context, '$e');
       } else {
         // ignore: use_build_context_synchronously
-        _showErrorSnackbar(context, 'Failed to pause simulation');
+        _showErrorSnackbar(
+            // ignore: use_build_context_synchronously
+            context,
+            // ignore: use_build_context_synchronously
+            AppLocalizations.of(context)!.simulation_particle_pause_failed);
       }
     }
   }
@@ -73,7 +82,11 @@ class _SimulationParticleCardState extends State<SimulationParticleCard> {
         _showErrorSnackbar(context, '$e');
       } else {
         // ignore: use_build_context_synchronously
-        _showErrorSnackbar(context, 'Failed to stop simulation');
+        _showErrorSnackbar(
+            // ignore: use_build_context_synchronously
+            context,
+            // ignore: use_build_context_synchronously
+            AppLocalizations.of(context)!.simulation_particle_stop_failed);
       }
     }
   }
@@ -88,33 +101,43 @@ class _SimulationParticleCardState extends State<SimulationParticleCard> {
 
   @override
   Widget build(BuildContext context) {
+    final localizations = AppLocalizations.of(context)!;
+
     return ExpansionTile(
       leading: Icon(widget.icon),
       title: Text(widget.title),
       children: [
         TextFormField(
-          decoration: const InputDecoration(
-            labelText: 'Number of particles',
-            hintText: 'Enter number of particles',
+          decoration: InputDecoration(
+            labelText:
+                localizations.simulation_particle_number_Of_particles_label,
+            hintText: localizations
+                .simulation_particle_enter_number_Of_particles_hint,
           ),
           keyboardType: TextInputType.number,
         ),
         TextFormField(
-          decoration: const InputDecoration(
-            labelText: 'Time step',
-            hintText: 'Enter cycle rate of main life time',
+          decoration: InputDecoration(
+            labelText: localizations.simulation_particle_time_step_label,
+            hintText: localizations
+                .simulation_particle_enter_cycle_rate_Of_main_lifetime_hint,
           ),
           keyboardType: TextInputType.number,
         ),
         const SizedBox(height: 16),
         ElevatedButton(
           onPressed: _isStarted ? (_isPaused ? _start : _pause) : _start,
-          child:
-              Text(_isPaused ? 'Continue' : (_isStarted ? 'Pause' : 'Start')),
+          child: Text(
+            _isPaused
+                ? localizations.simulation_particle_continue_button
+                : (_isStarted
+                    ? localizations.simulation_particle_pause_button
+                    : localizations.simulation_particle_start_button),
+          ),
         ),
         ElevatedButton(
           onPressed: _stop,
-          child: const Text('Stop'),
+          child: Text(localizations.simulation_particle_stop_button),
         ),
       ],
     );
